29636a1e844143e2c3ce6225e6a7748367b5fbdd
Martin0809 <maning0809@gmail.com>
2020-11-10 15:21:31 +0800

    feat: add inquire

--- src/init/index.js
+++ src/init/index.js
@@ -2,6 +2,7 @@ const Middleware = require('../helpers/middleware')
 const confirm = require('./confirm')
 const resolve = require('./resolve')
 const load = require('./load')
+const inquire = require('./inquire')
 const complete = require('./complete')
 
 const middleware = new Middleware()
@@ -10,8 +11,10 @@ const middleware = new Middleware()
 middleware.use(confirm)
 // 处理模版文件
 middleware.use(resolve)
-// 加载模版中配置
+// 加载模版中的配置
 middleware.use(load)
+// 执行模版中的命令行交互
+middleware.use(inquire)
 // 完成
 middleware.use(complete)
 
--- src/init/load.js
+++ src/init/load.js
@@ -1,12 +1,13 @@
 module.exports = (ctx) => {
   try {
+    // 获取 template 中 index.js 导出的对象
     const config = require(ctx.src)
 
     if (Object.prototype.toString.call(config) !== '[object Object]') {
       throw new TypeError('Template needs to expose an object.')
     }
 
-    ctx.config = config
+    Object.assign(ctx.config, config)
 
     return ctx
   } catch (err) {
